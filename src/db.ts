bc65575ff1854508863446504db4a7413ddce3c3
mavix21 <marcevizcarra2109@hotmail.com>
2024-12-12 00:23:18 -0500

    Create runnable program for testing purposes

--- src/db.ts
+++ src/db.ts
@@ -1,69 +1,31 @@
-import { encodeBase64Url } from "jsr:@std/encoding";
-import { crypto } from "jsr:@std/crypto/crypto";
+import { Console, Effect } from "effect";
+import { LinkRepositoryContext } from "./features/links/domain/link-repository.ts";
+import { generateShortCode } from "./features/links/domain/generate-short-code.ts";
+import { DenoKvLinkRepository } from "./features/links/infrastructure/deno-kv-link-repository.ts";
 
-export async function generateShortCode(longUrl: string) {
-  try {
-    new URL(longUrl);
-  } catch (error) {
-    console.error(error);
-    throw new Error("Invalid URL");
-  }
+const program = Effect.gen(function* () {
+  const longUrl = "https://deno.land/manual";
+  const shortCode = yield* generateShortCode(longUrl);
+  const linkRepository = yield* LinkRepositoryContext;
+  const userId = "test";
 
-  // Generate an unique identifier for the URL
-  const urlData = new TextEncoder().encode(longUrl + Date.now());
-  const hash = await crypto.subtle.digest("SHA-256", urlData);
-
-  // Take the first 8 of the hash for the short URL
-  return encodeBase64Url(hash.slice(0, 8));
-}
-
-const kv = await Deno.openKv();
-
-export type ShortLink = {
-  shortCode: string;
-  longUrl: string;
-  createdAt: number;
-  userId: string;
-  clickCount: number;
-  lastClickedEvent?: string;
-};
-
-export async function storeShortLink(
-  longUrl: string,
-  shortCode: string,
-  userId: string,
-) {
-  const shortLinkKey = ["shortLinks", shortCode];
-  const data: ShortLink = {
-    shortCode,
+  yield* Console.log("Before storing link");
+  yield* linkRepository.storeShortLink({
     longUrl,
     userId,
-    createdAt: Date.now(),
     clickCount: 0,
-  };
+    shortCode,
+    createdAt: Date.now(),
+  });
 
-  const res = await kv.set(shortLinkKey, data);
+  const link = yield* linkRepository.getShortLink(shortCode);
+  yield* Console.log(link);
+});
 
-  if (!res) {
-    throw new Error("Failed to store short link");
-  }
+const runnable = Effect.provideService(
+  program,
+  LinkRepositoryContext,
+  new DenoKvLinkRepository(),
+);
 
-  return res;
-}
-
-export async function getShortLink(shortCode: string) {
-  const link = await kv.get<ShortLink>(["shortLinks", shortCode]);
-
-  return link.value;
-}
-
-const longUrl = "https://deno.land/manual";
-const shortCode = await generateShortCode(longUrl);
-const userId = "test";
-
-console.log(shortCode);
-
-await storeShortLink(longUrl, shortCode, userId);
-
-const link = await getShortLink(shortCode);
-console.log(link);
+Effect.runPromise(runnable);
