9f41164f510c11e8e167472b80df7a3366996bc4
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-10-29 16:48:15 -0500

    feat: add scratch file

---
+++ src/features/links/domain/scratch.ts
@@ -0,0 +1,48 @@
+type TValidatorFunction = Record<string, (value: string) => string | undefined>;
+
+const makeFormValidatorFactory = <TValidatorKeys extends string>(
+  validators: Record<TValidatorKeys, (value: string) => string | void>,
+) =>
+<TObjKeys extends string>(
+  config: Record<TObjKeys, Array<TValidatorKeys>>,
+) => {
+  return (values: Record<TObjKeys, string>) => {
+    const errors = {} as Record<TObjKeys, string | undefined>;
+
+    for (const key in config) {
+      for (const validator of config[key]) {
+        const error = validators[validator](values[key]);
+        if (error) {
+          errors[key] = error;
+          break;
+        }
+      }
+    }
+
+    return errors;
+  };
+};
+
+const createFormValidator = makeFormValidatorFactory({
+  required: (value) => {
+    if (value === "") {
+      return "Required";
+    }
+  },
+  minLength: (value) => {
+    if (value.length < 5) {
+      return "Minimum length is 5";
+    }
+  },
+  email: (value) => {
+    if (!value.includes("@")) {
+      return "Invalid email";
+    }
+  },
+});
+
+const validateUser = createFormValidator({
+  id: ["required"],
+  username: ["required", "minLength"],
+  email: ["required", "email"],
+});
